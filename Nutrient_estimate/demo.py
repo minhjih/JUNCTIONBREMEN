98494cf1f557c0600ffe8c984efdf565f6ecb921
Minji <87082053+minhjih@users.noreply.github.com>
2024-08-11 02:25:26 +0900

    Update demo.py

--- Nutrient_estimate/demo.py
+++ Nutrient_estimate/demo.py
@@ -7,23 +7,13 @@ import modules, net, resnet, densenet, senet
 import numpy as np
 import loaddata_demo as loaddata
 import pdb
-import argparse
+
 from volume import get_volume
 from mask import get_mask
 
 import matplotlib.image
 import matplotlib.pyplot as plt
 from get_json_from_image import get_food_json
-
-parser = argparse.ArgumentParser(description='KD-network')
-parser.add_argument('--img', metavar='DIR',default="./input/test.jpg",
-                    help='img to input')
-parser.add_argument('--json', metavar='DIR',default="./input/test.json",
-                    help='json file to input')
-parser.add_argument('--output', metavar='DIR',default="./output",
-                    help='dir to output')
-
-args=parser.parse_args()
 
 def define_model(is_resnet, is_densenet, is_senet):
     if is_resnet:
@@ -42,26 +32,27 @@ def define_model(is_resnet, is_densenet, is_senet):
     return model
    
 
-def main():
-    if (not os.path.exists(args.output)):
+def run(img_loc = "./input.jpg"):
+    if (not os.path.exists("./output")):
         print("Output directory doesn't exist! Creating...")
-        os.makedirs(args.output)
+        os.makedirs("./output")
 
     model = define_model(is_resnet=False, is_densenet=False, is_senet=True)
     model = torch.nn.DataParallel(model).cuda()
     model.load_state_dict(torch.load('./pretrained_model/model_senet'))
     model.eval()
-    print
-    img = cv2.imread(args.img)
+ 
+    img = cv2.imread(img_loc)
+
+    get_food_json(img, img_loc) # food json masking 영역 가져오기
     
-    nyu2_loader = loaddata.readNyu2(args.img)
+    nyu2_loader = loaddata.readNyu2(img_loc)
   
     test(nyu2_loader, model, img.shape[1], img.shape[0])
-
+    
 
 def test(nyu2_loader, model, width, height):
     for i, image in enumerate(nyu2_loader):
-        
         image = torch.autograd.Variable(image, volatile=True).cuda()
         out = model(image)
         out = out.view(out.size(2),out.size(3)).data.cpu().numpy()
@@ -69,22 +60,25 @@ def test(nyu2_loader, model, width, height):
         min_pix = out.min()
         out = (out-min_pix)/(max_pix-min_pix)*255
         out = cv2.resize(out,(width,height),interpolation=cv2.INTER_CUBIC)
-        cv2.imwrite(os.path.join(args.output, "out_grey.png"),out)
-        out_grey = cv2.imread(os.path.join(args.output, "out_grey.png"),0)
+        cv2.imwrite(os.path.join("./output", "out_grey.png"),out)
+        out_grey = cv2.imread(os.path.join("./output", "out_grey.png"),0)
         out_color = cv2.applyColorMap(out_grey, cv2.COLORMAP_JET)
-        cv2.imwrite(os.path.join(args.output, "out_color.png"),out_color)
-        vol = get_volume(out_grey, args.json)
+        cv2.imwrite(os.path.join("./output", "out_color.png"),out_color)
+        vol = get_volume(out_grey, "./labelme_annotations_ellipse.json")
         print("Volume:")
         print(vol)
         print("unit: cm^3")
-        out_file = open(os.path.join(args.output, "out.txt"), "w")
+
+        """
+        vol 딕셔너리로 volume 나타내는거 -> 테이블 계산하기
+        """
+        out_file = open(os.path.join("./output", "out.txt"), "w")
         out_file.write("Volume:\n")
         out_file.write(str(vol))
         out_file.write("\n")
         out_file.write("unit: cm^3")
         out_file.close()
-        get_mask(out_grey, args.json, args.output)
-        
-        
-if __name__ == '__main__':
-    main()
+        get_mask(out_grey, "./labelme_annotations_ellipse.json", "./output")
+
+if __name__ == "__main__":
+    run("./test/images/IMG_5808_jpeg.rf.2679f78fabc95e90c79cb1702fea98be.jpg")
