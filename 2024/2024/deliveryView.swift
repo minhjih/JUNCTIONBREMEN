05a426b8ebab12f9462884cb71ed4fd83bb1dc16
Jiwoo Hong <jiwooh@postech.ac.kr>
2024-08-11 03:09:29 +0900

    delivery 섹션 완성

--- 2024/2024/deliveryView.swift
+++ 2024/2024/deliveryView.swift
@@ -19,10 +19,10 @@ struct deliveryView: View {
                         NavigationLink(destination: deliveryMenuView()) {
                                 RestaurantCard(image1: "rest1_1", image2: "rest1_2", image3: "rest1_3", name: "Chicken Table", rating: "4.5", time: "40-45", tip: "2-4", min: "9.99")
                         }
+                        RestaurantCard(image1: "rest3_1", image2: "rest3_2", image3: "rest3_3", name: "Donburi", rating: "3.9", time: "25-40", tip: "1-3", min: "8.99")
+                        RestaurantCard(image1: "rest4_1", image2: "rest4_2", image3: "rest4_3", name: "Salad Bowls", rating: "4.8", time: "20-30", tip: "1-4", min: "8.99")
                         RestaurantCard(image1: "rest1_1", image2: "rest1_2", image3: "rest1_3", name: "Chicken Table", rating: "4.5", time: "40-45", tip: "2-4", min: "9.99")
-                        RestaurantCard(image1: "rest1_1", image2: "rest1_2", image3: "rest1_3", name: "Chicken Table", rating: "4.5", time: "40-45", tip: "2-4", min: "9.99")
-                        RestaurantCard(image1: "rest1_1", image2: "rest1_2", image3: "rest1_3", name: "Chicken Table", rating: "4.5", time: "40-45", tip: "2-4", min: "9.99")
-                        RestaurantCard(image1: "rest1_1", image2: "rest1_2", image3: "rest1_3", name: "Chicken Table", rating: "4.5", time: "40-45", tip: "2-4", min: "9.99")
+                        RestaurantCard(image1: "rest4_1", image2: "rest4_2", image3: "rest4_3", name: "Salad Bowls", rating: "4.8", time: "20-30", tip: "1-4", min: "8.99")
                     }
                 }
             }
