a0eafeff21a476a324cc64cfcf7aca3932a510b9
Jiwoo Hong <jiwooh@postech.ac.kr>
2024-08-10 21:05:55 +0900

    내비게이션 바 타이틀 추가

--- 2024/2024/homeView.swift
+++ 2024/2024/homeView.swift
@@ -7,31 +7,84 @@
 
 import SwiftUI
 
+enum tabs: Hashable {
+    case home
+    case delivery
+    case chat
+    case stats
+}
+
 struct homeView: View {
+    @State private var sel = tabs.home
     var body: some View {
         NavigationStack {
-            TabView {
+            TabView(selection: $sel) {
                 mainView()
                     .tabItem {
                         Image(systemName: "house")
                         Text("Home")
                     }
+                    .tag(tabs.home)
                 deliveryView()
                     .tabItem {
                         Image(systemName: "fork.knife")
                         Text("Delivery")
                     }
+                    .tag(tabs.delivery)
                 ChatView()
                     .tabItem {
                         Image(systemName: "magnifyingglass")
                         Text("Chatbot")
                     }
+                    .tag(tabs.chat)
                 statsView()
                     .tabItem {
                         Image(systemName: "chart.bar.xaxis.ascending")
                         Text("Statistics")
                     }
-            }.accentColor(.priMain)
+                    .tag(tabs.stats)
+            }
+            .accentColor(.priMain)
+            .toolbar {
+                switch sel {
+                case .home:
+                    ToolbarItem(placement: .principal) {
+                        Text("Home")
+                            .foregroundColor(.priMain)
+                            .fontWeight(.black)
+                            .italic()
+                            .underline()
+        //                    .shadow(color: Color.priMain.opacity(0.5), radius: 15, x: 0, y: 0)
+                    }
+                case .delivery:
+                    ToolbarItem(placement: .principal) {
+                        Text("Delivery")
+                            .foregroundColor(.priMain)
+                            .fontWeight(.black)
+                            .italic()
+                            .underline()
+        //                    .shadow(color: Color.priMain.opacity(0.5), radius: 15, x: 0, y: 0)
+                    }
+                case .chat:
+                    ToolbarItem(placement: .principal) {
+                        Text("Chatbot")
+                            .foregroundColor(.priMain)
+                            .fontWeight(.black)
+                            .italic()
+                            .underline()
+        //                    .shadow(color: Color.priMain.opacity(0.5), radius: 15, x: 0, y: 0)
+                    }
+                case .stats:
+                    ToolbarItem(placement: .principal) {
+                        Text("Statistics")
+                            .foregroundColor(.priMain)
+                            .fontWeight(.black)
+                            .italic()
+                            .underline()
+        //                    .shadow(color: Color.priMain.opacity(0.5), radius: 15, x: 0, y: 0)
+                    }
+                }
+            }
         }
     }
 }
